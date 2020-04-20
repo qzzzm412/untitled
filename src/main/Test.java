14419b1bc3de1c98ba367163ae877b512808d4de
958430327@qq.com <958430327@qq.com>
2020-04-20 10:21:14 +0800

    第一次测试idea

---
+++ src/main/Test.java
@@ -0,0 +1,12 @@
+package main;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.sql.SQLOutput;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
